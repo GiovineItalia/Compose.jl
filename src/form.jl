d22ea05d9c898a81b35aaa768a29273025edb60d
Daniel Jones <dcjones@cs.washington.edu>
2014-07-21 14:13:35 -0700

    Start implementing a path type.

--- src/form.jl
+++ src/form.jl
@@ -379,3 +379,307 @@ function absolute_units(p::BitmapPrimitive, t::Transform, units::UnitBox,
 end
 
 
+# Path
+# ----
+
+# An implementation of the SVG path mini-language.
+
+abstract PathOp
+
+immutable MoveAbsPathOp <: PathOp
+    to::Point
+end
+
+function assert_pathop_tokens_len(op_type, tokens, i, needed)
+    provided = length(tokens) - i + 1
+    if provided < needed
+        error("In path $(op_type) requires $(needed) argumens but only $(provided) provided.")
+    end
+end
+
+function parsepathop(::Type{MoveAbsPathOp}, tokens::AbstractArray, i)
+    assert_pathop_tokens_len(MoveAbsPathOp, tokens, i, 2)
+    op = MoveAbsPathOp(Point(tokens[i], tokens[i + 1]))
+    return (op, i + 2)
+end
+
+
+immutable MoveRelPathOp <: PathOp
+    to::Point
+end
+
+function parsepathop(::Type{MoveRelPathOp}, tokens::AbstractArray, i)
+    assert_pathop_tokens_len(MoveAbsPathOp, tokens, i, 2)
+    op = MoveAbsPathOp(Point(tokens[i], tokens[i + 1]))
+    return (op, i + 2)
+end
+
+
+immutable ClosePathOp <: PathOp
+end
+
+function parsepathop(::Type{ClosePathOp}, tokens::AbstractArray, i)
+    return (ClosePathOp(), i)
+end
+
+
+immutable LineAbsPathOp <: PathOp
+    to::Point
+end
+
+function parsepathop(::Type{LineAbsPathOp}, tokens::AbstractArray, i)
+    assert_pathop_tokens_len(LineAbsPathOp, tokens, i, 2)
+    op = LineAbsPathOp(Point(tokens[i], tokens[i + 1]))
+    return (op, i + 2)
+end
+
+
+immutable LineRelPathOp <: PathOp
+    to::Point
+end
+
+function parsepathop(::Type{LineRelPathOp}, tokens::AbstractArray, i)
+    assert_pathop_tokens_len(LineRelPathOp, tokens, i, 2)
+    op = LineRelPathOp(Point(tokens[i], tokens[i + 1]))
+    return (op, i + 2)
+end
+
+
+immutable HorLineAbsPathOp <: PathOp
+    x::Measure
+end
+
+function parsepathop(::Type{HorLineAbsPathOp}, tokens::AbstractArray, i)
+    assert_pathop_tokens_len(HorLineAbsPathOp, tokens, i, 1)
+    op = HorLineAbsPathOp(x_measure(tokens[i]))
+    return (op, i + 1)
+end
+
+
+immutable HorLineRelPathOp <: PathOp
+    Δx::Measure
+end
+
+function parsepathop(::Type{HorLineRelPathOp}, tokens::AbstractArray, i)
+    assert_pathop_tokens_len(HorLineRelPathOp, tokens, i, 1)
+    op = HorLineRelPathOp(x_measure(tokens[i]))
+    return (op, i + 1)
+end
+
+
+immutable VertLineAbsPathOp <: PathOp
+    y::Measure
+end
+
+function parsepathop(::Type{VertLineAbsPathOp}, tokens::AbstractArray, i)
+    assert_pathop_tokens_len(VertLineAbsPathOp, tokens, i, 1)
+    op = VertLineAbsPathOp(y_measure(tokens[i]))
+    return (op, i + 1)
+end
+
+
+immutable VertLineRelPathOp <: PathOp
+    Δy::Measure
+end
+
+function parsepathop(::Type{VertLineRelPathOp}, tokens::AbstractArray, i)
+    assert_pathop_tokens_len(VertLineRelPathOp, tokens, i, 1)
+    op = VertLineRelPathOp(y_measure(tokens[i]))
+    return (op, i + 1)
+end
+
+
+immutable CubicCurveAbsPathOp <: PathOp
+    ctrl1::Point
+    ctrl2::Point
+    to::Point
+end
+
+function parsepathop(::Type{CubicCurveAbsPathOp}, tokens::AbstractArray, i)
+    assert_pathop_tokens_len(CubicCurveAbsPathOp, tokens, i, 6)
+    op = CubicCurveAbsPathOp(Point(tokens[i],     tokens[i + 1]),
+                             Point(tokens[i + 2], tokens[i + 3]),
+                             Point(tokens[i + 4], tokens[i + 5]))
+    return (op, i + 6)
+end
+
+
+immutable CubicCurveRelPathOp <: PathOp
+    ctrl1::Point
+    ctrl2::Point
+    to::Point
+end
+
+function parsepathop(::Type{CubicCurveRelPathOp}, tokens::AbstractArray, i)
+    assert_pathop_tokens_len(CubicCurveRelPathOp, tokens, i, 6)
+    op = CubicCurveRelPathOp(Point(tokens[i],     tokens[i + 1]),
+                             Point(tokens[i + 2], tokens[i + 3]),
+                             Point(tokens[i + 4], tokens[i + 5]))
+    return (op, i + 6)
+end
+
+
+immutable QuadCurveAbsPathOp <: PathOp
+    ctrl1::Point
+    to::Point
+end
+
+function parsepathop(::Type{QuadCurveAbsPathOp}, tokens::AbstractArray, i)
+    assert_pathop_tokens_len(QuadCurveAbsPathOp, tokens, i, 4)
+    op = QuadCurveAbsPathOp(Point(tokens[i],     tokens[i + 1]),
+                            Point(tokens[i + 2], tokens[i + 3]))
+    return (op, i + 4)
+end
+
+
+immutable QuadCurveRelPathOp <: PathOp
+    ctrl1::Point
+    to::Point
+end
+
+function parsepathop(::Type{QuadCurveRelPathOp}, tokens::AbstractArray, i)
+    assert_pathop_tokens_len(QuadCurveRelPathOp, tokens, i, 4)
+    op = QuadCurveRelPathOp(Point(tokens[i],     tokens[i + 1]),
+                            Point(tokens[i + 2], tokens[i + 3]))
+    return (op, i + 4)
+end
+
+
+immutable QuadCurveShortAbsPathOp <: PathOp
+    to::Point
+end
+
+function parsepathop(::Type{QuadCurveShortAbsPathOp}, tokens::AbstractArray, i)
+    assert_pathop_tokens_len(QuadCurveShortAbsPathOp, tokens, i, 2)
+    op = QuadCurveShortAbsPathOp(Point(tokens[i], tokens[i + 1]))
+    return (op, i + 2)
+end
+
+
+immutable QuadCurveShortRelPathOp <: PathOp
+    to::Point
+end
+
+function parsepathop(::Type{QuadCurveShortRelPathOp}, tokens::AbstractArray, i)
+    assert_pathop_tokens_len(QuadCurveShortRelPathOp, tokens, i, 2)
+    op = QuadCurveShortRelPathOp(Point(tokens[i], tokens[i + 1]))
+    return (op, i + 2)
+end
+
+
+immutable ArcAbsPathOp <: PathOp
+    rx::Measure
+    ry::Measure
+    rotation::Float64
+    largearc::Bool
+    sweep::Bool
+    to::Point
+end
+
+immutable ArcRelPathOp <: PathOp
+    rx::Measure
+    ry::Measure
+    rotation::Float64
+    largearc::Bool
+    sweep::Bool
+    to::Point
+end
+
+function parsepathop{T <: Union(ArcAbsPathOp, ArcRelPathOp)}(::Type{T}, tokens::AbstractArray, i)
+    assert_pathop_tokens_len(T, tokens, i, 7)
+
+    if isa(tokens[4], Bool)
+        largearc = tokens[4]
+    elseif tokens[4] == 0
+        largearc = false
+    elseif tokens[4] == 1
+        largearc = true
+    else
+        error()
+    end
+
+    if isa(tokens[5], Bool)
+        sweep = tokens[5]
+    elseif tokens[5] == 0
+        sweep = false
+    elseif tokens[5] == 1
+        sweep = true
+    else
+        error()
+    end
+
+    if !isa(tokens[i + 2], Number)
+        error("path arc operation requires a numerical rotation")
+    end
+
+    op = T(x_measure(tokens[i]),
+           y_measure(tokens[i + 1]),
+           convert(Float64, tokens[i + 2]),
+           largearc, sweep,
+           Point(tokens[5], tokens[6]))
+
+    return (op, i + 7)
+end
+
+
+const path_ops = [
+     :M => MoveAbsPathOp,
+     :m => MoveRelPathOp,
+     :Z => ClosePathOp,
+     :z => ClosePathOp,
+     :L => LineAbsPathOp,
+     :l => LineRelPathOp,
+     :H => HorLineAbsPathOp,
+     :h => HorLineAbsPathOp,
+     :V => VertLineAbsPathOp,
+     :v => VertLineRelPathOp,
+     :C => CubicCurveAbsPathOp,
+     :c => CubicCurveRelPathOp,
+     :Q => QuadCurveAbsPathOp,
+     :q => QuadCurveRelPathOp,
+     :T => QuadCurveShortAbsPathOp,
+     :t => QuadCurveShortRelPathOp,
+     :A => ArcAbsPathOp,
+     :a => ArcRelPathOp
+]
+
+
+# A path is an array of symbols, numbers, and measures following SVGs path
+# mini-language.
+function parsepath(tokens::AbstractArray)
+    ops = PathOp[]
+    last_op_type = nothing
+    i = 1
+    while i <= length(tokens)
+        tok = tokens[i]
+        if isa(tok, Symbol)
+            if !haskey(path_ops, tok)
+                error("$(tok) is not a valid path operation")
+            else
+                op_type = path_ops[tok]
+                i += 1
+                op, i = parsepathop(op_type, tokens, i)
+                push!(ops, op)
+                last_op_type
+            end
+        else
+            i += 1
+            op, i = parsepathop(last_op_type, tokens, i)
+        end
+    end
+
+    return ops
+end
+
+
+
+
+immutable PathPrimitive <: FormPrimitive
+
+end
+
+
+
+
+
+
