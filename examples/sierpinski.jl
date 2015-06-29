8f64bcbf47aabf90ad9bc34e57a86127af68f81d
Daniel Jones <dcjones@cs.washington.edu>
2015-06-29 09:53:27 -0700

    Fix CirclePrimitive.

--- examples/sierpinski.jl
+++ examples/sierpinski.jl
@@ -1,14 +1,16 @@
 using Compose
 
+const padding = 0.5mm
+
 function sierpinski(n::Int)
     if n == 0
         compose(context(), polygon([(1,1), (0,1), (1/2, 0)]))
     else
         t = sierpinski(n - 1)
         compose(context(),
-                (context(1/4,   0, 1/2, 1/2), t),
-                (context(  0, 1/2, 1/2, 1/2), t),
-                (context(1/2, 1/2, 1/2, 1/2), t))
+                (context(0.25w + padding, padding,        0.5w - 2*padding, 0.5h - 2*padding), t),
+                (context(padding,         0.5h + padding, 0.5w - 2*padding, 0.5h - 2*padding), t),
+                (context(0.5w + padding,  0.5h + padding, 0.5w - 2*padding, 0.5h - 2*padding), t))
     end
 end
 
