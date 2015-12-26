6965b08cf5747f5a5ae8ed527be98aac828fc316
George Lesica <george@lesica.com>
2015-12-25 18:50:04 -0700

    Add test case.

---
+++ examples/text.jl
@@ -0,0 +1,9 @@
+#!/usr/bin/env julia
+
+# Draw some text to a PNG image.
+
+using Compose
+
+img = PNG("text.png", 400px, 400px)
+c = compose(compose(context(), text(150px, 200px, "hello & goodbye")), fill("tomato"))
+draw(img, c)
