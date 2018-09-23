ddaff21d454a1cde3f8a3d898b3e635bc4d700c8
Ben Arthur <arthurb@hhmi.org>
2018-09-23 18:22:40 -0400

    abandon current_point()

--- src/cairo_backends.jl
+++ src/cairo_backends.jl
@@ -457,14 +457,6 @@ apply_property(img::Image, property::SVGAttributePrimitive) = nothing
 # Cairo Wrappers
 # --------------
 
-function current_point(img::Image)
-    x = Array{Float64}(undef, 1)
-    y = Array{Float64}(undef, 1)
-    ccall((:cairo_get_current_point, Cairo._jl_libcairo), Cvoid,
-          (Ptr{Cvoid}, Ptr{Float64}, Ptr{Float64}), img.ctx.ptr, x, y)
-    return ((x[1] / img.ppmm)*mm, (x[2] / img.ppmm)*mm)
-end
-
 move_to(img::Image, point::AbsoluteVec2) = Cairo.move_to(img.ctx,
         absolute_native_units(img, point[1].value),
         absolute_native_units(img, point[2].value))
