ac5f62d0e528e4414d6f9f70193e5279e3072041
Tamas Nagy <tamas@tamasnagy.com>
2018-10-21 10:30:06 -0700

    add testing for raw svg output

---
+++ test/svg.jl
@@ -0,0 +1,19 @@
+using Test
+using EzXML
+using Colors
+using Measures
+
+@testset "Issue 267" begin
+    c = compose(context(), fill(["red", "blue"]),
+                [context(), fill("green"), circle([0.25, 0.75], [0.5], [0.25])])
+    img = SVG(8cm, 6cm, false)
+    draw(img, c)
+    svgxml = root(parsexml(String(img.out.data)))
+    # get all grouped values that have the fill attribute
+    fillcolors = nodecontent.(findall("//ns:g[@fill]/@fill", svgxml, ["ns"=>namespace(svgxml)]))
+    # there should only be a single color (because green should clobber the red
+    # and blue colors)
+    @test length(fillcolors) == 1
+    # make sure it's green
+    @test fillcolors[1] == "rgba(0,128,0,1)"
+end
