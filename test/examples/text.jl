47bd9c9b8b4490b40a3adcfdf8246b6781cc6ce8
Ben Arthur <arthurb@hhmi.org>
2020-03-24 11:46:57 -0400

    fix pango bug with unicode text

--- test/examples/text.jl
+++ test/examples/text.jl
@@ -5,5 +5,8 @@
 using Compose
 
 img = PNG("text.png", 400px, 400px)
-c = compose(compose(context(), text(150px, 200px, "hello &amp; goodbye")), fill("tomato"))
+c = compose(compose(context(),
+                    text(150px, 200px,
+                         "hello &amp; goodbye\nFoo<sub>Sub</sub>Bar<sup>Sup</sup>")),
+            fill("tomato"))
 draw(img, c)
